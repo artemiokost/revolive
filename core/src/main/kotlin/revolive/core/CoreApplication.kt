3c0a332aa2a6cbacae9b7364e223ad89ac95b4cb
artemiokost <artemiokost@gmail.com>
2024-10-10 20:38:08 +0700

    revolive -- fin

--- core/src/main/kotlin/revolive/core/CoreApplication.kt
+++ core/src/main/kotlin/revolive/core/CoreApplication.kt
@@ -1,6 +1,48 @@
 package revolive.core
 
-class CoreApplication
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.random.Random
+
+object LoadBalancer {
+
+    val WARN_EXISTS = "Such instance already exists!"
+    val WARN_CAPACITY = "No more space!"
+    val STORAGE_CAPACITY = 10
+
+    private val INSTANCE_STORAGE = ConcurrentHashMap<String, Int>(STORAGE_CAPACITY)
+
+    fun register(instance: String) {
+        if (INSTANCE_STORAGE.size == STORAGE_CAPACITY) {
+            throw RuntimeException(WARN_CAPACITY)
+        }
+        INSTANCE_STORAGE[instance]?.let {
+            throw RuntimeException(WARN_EXISTS)
+        }
+        INSTANCE_STORAGE[instance] = INSTANCE_STORAGE.size + 1
+    }
+
+    fun getAllRegistered(): Enumeration<String>? {
+        return INSTANCE_STORAGE.keys()
+    }
+
+    fun getRandom(): String? {
+        val keys = INSTANCE_STORAGE.keys().toList()
+        val rand = Random.nextInt(keys.size)
+        return keys[rand]
+    }
+}
+
+fun amountToReachLimit(l: BigDecimal, r: BigDecimal): BigDecimal {
+    return BigDecimal.ONE.subtract(l).max(BigDecimal.ZERO).min(r).setScale(2, RoundingMode.HALF_UP)
+}
+
+fun BigDecimal.amountToReachLimit(other: BigDecimal, limit: BigDecimal = BigDecimal.ONE): BigDecimal {
+    val result = limit.subtract(this).max(BigDecimal.ZERO).min(other)
+    return result.setScale(2, RoundingMode.HALF_UP)
+}
 
 fun main(args: Array<String>) {
     println("Biba boba")
--- core/src/test/kotlin/revolive/core/CoreTest.kt
+++ core/src/test/kotlin/revolive/core/CoreTest.kt
@@ -1,11 +1,40 @@
 package revolive.core
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import revolive.core.LoadBalancer.WARN_CAPACITY
+import revolive.core.LoadBalancer.WARN_EXISTS
+import kotlin.test.assertTrue
 
 class CoreTest {
 
     @Test
-    fun test() {
+    fun testRegistration() {
+        val firstPart = (1..10).map { "https://test$it.com" }
+        val secondPart = (11..20).map { "https://test$it.com" }
 
+        firstPart.forEach { LoadBalancer.register(it) }
+
+        val random1 = firstPart.random()
+        assertThrows<RuntimeException>(WARN_EXISTS) {
+            LoadBalancer.register(random1)
+        }
+
+        val random2 = secondPart.random()
+        assertThrows<RuntimeException>(WARN_CAPACITY) {
+            LoadBalancer.register(random2)
+        }
+    }
+
+    @Test
+    fun testGetter() {
+        val firstPart = (1..10).map { "https://test$it.com" }
+
+        firstPart.forEach { LoadBalancer.register(it) }
+
+        val random = LoadBalancer.getRandom()
+        assertTrue {
+            firstPart.contains(random)
+        }
     }
 }
